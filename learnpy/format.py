d1b71042943a322b095b0d90263fd951b87b079f
rootpip <306127031@qq.com>
2017-02-06 17:08:21 +0800

    20170206-0.1.1

--- learnpy/format.py
+++ learnpy/format.py
@@ -1,7 +1,9 @@
 # try [] in .format
-somelist =  list('SPAM')
-x = [0, 100] #以列表的形式生成变量
+somelist = list('SPAM')
+x = list(range(0,int(input())))  # 以列表的形式生成变量
 x[0] = 'first = {0[0]}, third = {0[2]}'.format(somelist)
-x[1] = 'first = {0}, last = {1}'.format(somelist[0],somelist[-1])
+x[1] = 'first = {0}, last = {1}'.format(somelist[0], somelist[-1])
+parts = somelist[0], somelist[-1], somelist[1:3]
+x[2] = 'first = {0}, last = {1}, middle = {2}'.format(*parts)
 for i in x:
     print(i)
